8179e7164922a540337d8cf4efd0eb1cbf32509b
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-14 12:56:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,20 @@
 def game_hash
   
-  {}
+  {
+    :home=> {
+      :team_name=> "Brooklyn Nets",
+      :colors=> ["Black"," White"],
+      :players=> [
+        
+        ]
+      }, 
+    :away=> {
+      :team_name=> "",
+      :colors=> ["Turquoise","Purple"],
+      :players=>
+        
+      }
+  }
 end
 
 
