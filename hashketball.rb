16628f4e7e8a2ad3388a17f87fc9525edb4f0ed9
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-14 13:04:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,11 +5,20 @@ def game_hash
       :team_name=> "Brooklyn Nets",
       :colors=> ["Black"," White"],
       :players=> [
-        
+        {:player_name=>"Alan Anderson",
+        :number=>0,
+        :shoe=>16,
+        :points=>22,
+        :rebounds=>12,
+        :assists=>12,
+        :steals=>3,
+        :blocks=>1,
+        :slam_dunks=>1 },
+        {
         ]
       }, 
     :away=> {
-      :team_name=> "",
+      :team_name=> "Charlotte Hornets",
       :colors=> ["Turquoise","Purple"],
       :players=>[
         
